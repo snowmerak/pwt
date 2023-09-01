946b5af3e865f4b7a4eb875ed9c66d2195e31445
snowmerak <snowmerak@outlook.com>
2023-09-02 00:10:33 +0900

    make payload and types

---
+++ lib/pwt/payload.go
@@ -0,0 +1,281 @@
+package pwt
+
+import (
+	"encoding/binary"
+	"errors"
+	"github.com/snowmerak/pwt/gen/grpc/model/token"
+	"google.golang.org/protobuf/proto"
+	"math"
+)
+
+func AddPayloads[T PayloadTypes](pwt *PWT, key string, value T) error {
+	switch v := any(value).(type) {
+	case int:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_INT,
+			Value: buf,
+		}
+	case int8:
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_INT8,
+			Value: []byte{byte(v)},
+		}
+	case int16:
+		buf := make([]byte, 2)
+		binary.BigEndian.PutUint16(buf, uint16(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_INT16,
+			Value: buf,
+		}
+	case int32:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, uint32(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_INT32,
+			Value: buf,
+		}
+	case int64:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_INT64,
+			Value: buf,
+		}
+	case uint:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_UINT,
+			Value: buf,
+		}
+	case uint8:
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_UINT8,
+			Value: []byte{byte(v)},
+		}
+	case uint16:
+		buf := make([]byte, 2)
+		binary.BigEndian.PutUint16(buf, uint16(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_UINT16,
+			Value: buf,
+		}
+	case uint32:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, uint32(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_UINT32,
+			Value: buf,
+		}
+	case uint64:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, uint64(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_UINT64,
+			Value: buf,
+		}
+	case float32:
+		buf := make([]byte, 4)
+		binary.BigEndian.PutUint32(buf, math.Float32bits(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_FLOAT32,
+			Value: buf,
+		}
+	case float64:
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, math.Float64bits(v))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_FLOAT64,
+			Value: buf,
+		}
+	case string:
+		buf := make([]byte, 4+len(v))
+		binary.BigEndian.PutUint32(buf, uint32(len(v)))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_STRING,
+			Value: append(buf, []byte(v)...),
+		}
+	case []byte:
+		buf := make([]byte, 4+len(v))
+		binary.BigEndian.PutUint32(buf, uint32(len(v)))
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_BYTES,
+			Value: append(buf, v...),
+		}
+	case bool:
+		a := byte(0)
+		if v {
+			a = 1
+		}
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_BOOL,
+			Value: []byte{byte(a)},
+		}
+	case proto.Message:
+		buf, err := proto.Marshal(v)
+		if err != nil {
+			return err
+		}
+		pwt.token.Payloads[key] = &token.Payload{
+			Type:  token.PayloadType_MESSAGE,
+			Value: buf,
+		}
+	default:
+		return errInvalidPayloadType
+	}
+
+	return nil
+}
+
+func GetPayloads[T PayloadTypes](pwt *PWT, key string) (T, error) {
+	r := any(*new(T))
+	payload, ok := pwt.token.Payloads[key]
+	if !ok {
+		return r.(T), errors.New("invalid payload key")
+	}
+	switch r.(type) {
+	case int:
+		if payload.Type != token.PayloadType_INT {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 8 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = int(binary.BigEndian.Uint64(payload.Value))
+	case int8:
+		if payload.Type != token.PayloadType_INT8 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 1 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = int8(payload.Value[0])
+	case int16:
+		if payload.Type != token.PayloadType_INT16 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 2 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = int16(binary.BigEndian.Uint16(payload.Value))
+	case int32:
+		if payload.Type != token.PayloadType_INT32 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = int32(binary.BigEndian.Uint32(payload.Value))
+	case int64:
+		if payload.Type != token.PayloadType_INT64 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 8 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = int64(binary.BigEndian.Uint64(payload.Value))
+	case uint:
+		if payload.Type != token.PayloadType_UINT {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 8 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = uint(binary.BigEndian.Uint64(payload.Value))
+	case uint8:
+		if payload.Type != token.PayloadType_UINT8 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 1 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = uint8(payload.Value[0])
+	case uint16:
+		if payload.Type != token.PayloadType_UINT16 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 2 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = uint16(binary.BigEndian.Uint16(payload.Value))
+	case uint32:
+		if payload.Type != token.PayloadType_UINT32 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = uint32(binary.BigEndian.Uint32(payload.Value))
+	case uint64:
+		if payload.Type != token.PayloadType_UINT64 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 8 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = uint64(binary.BigEndian.Uint64(payload.Value))
+	case float32:
+		if payload.Type != token.PayloadType_FLOAT32 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = math.Float32frombits(binary.BigEndian.Uint32(payload.Value))
+	case float64:
+		if payload.Type != token.PayloadType_FLOAT64 {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 8 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = math.Float64frombits(binary.BigEndian.Uint64(payload.Value))
+	case string:
+		if payload.Type != token.PayloadType_STRING {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) < 4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		length := binary.BigEndian.Uint32(payload.Value)
+		if len(payload.Value) != int(length)+4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = string(payload.Value[4:])
+	case []byte:
+		if payload.Type != token.PayloadType_BYTES {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) < 4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		length := binary.BigEndian.Uint32(payload.Value)
+		if len(payload.Value) != int(length)+4 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = payload.Value[4:]
+	case bool:
+		if payload.Type != token.PayloadType_BOOL {
+			return r.(T), errInvalidPayloadType
+		}
+		if len(payload.Value) != 1 {
+			return r.(T), errInvalidPayloadLength
+		}
+		r = payload.Value[0] != 0
+	case proto.Message:
+		if payload.Type != token.PayloadType_MESSAGE {
+			return r.(T), errInvalidPayloadType
+		}
+		m := any(r).(proto.Message)
+		if err := proto.Unmarshal(payload.Value, m); err != nil {
+			return r.(T), err
+		}
+		r = m
+	default:
+		return r.(T), errInvalidPayloadType
+	}
+
+	return r.(T), nil
+}
---
+++ lib/pwt/payload_types.go
@@ -0,0 +1,64 @@
+package pwt
+
+import (
+	"github.com/snowmerak/pwt/gen/grpc/model/token"
+	"google.golang.org/protobuf/proto"
+)
+
+type PayloadTypes interface {
+	int
+	int8
+	int16
+	int32
+	int64
+	uint
+	uint8
+	uint16
+	uint32
+	uint64
+	float32
+	float64
+	string
+	[]byte
+	bool
+	proto.Message
+}
+
+func AccordPayloadType[T PayloadTypes](record token.PayloadType, value T) bool {
+	switch any(value).(type) {
+	case int:
+		return record == token.PayloadType_INT
+	case int8:
+		return record == token.PayloadType_INT8
+	case int16:
+		return record == token.PayloadType_INT16
+	case int32:
+		return record == token.PayloadType_INT32
+	case int64:
+		return record == token.PayloadType_INT64
+	case uint:
+		return record == token.PayloadType_UINT
+	case uint8:
+		return record == token.PayloadType_UINT8
+	case uint16:
+		return record == token.PayloadType_UINT16
+	case uint32:
+		return record == token.PayloadType_UINT32
+	case uint64:
+		return record == token.PayloadType_UINT64
+	case float32:
+		return record == token.PayloadType_FLOAT32
+	case float64:
+		return record == token.PayloadType_FLOAT64
+	case string:
+		return record == token.PayloadType_STRING
+	case []byte:
+		return record == token.PayloadType_BYTES
+	case bool:
+		return record == token.PayloadType_BOOL
+	case proto.Message:
+		return record == token.PayloadType_MESSAGE
+	}
+
+	return false
+}
