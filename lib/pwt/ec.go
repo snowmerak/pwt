1a8074b8472ce12550051fb223b48b07d204f6aa
snowmerak <snowmerak@outlook.com>
2023-09-02 02:23:38 +0900

    remake ec funcs

--- lib/pwt/ec.go
+++ lib/pwt/ec.go
@@ -29,7 +29,7 @@ func getECPrivateKeyFromPem(secret []byte) (*ecdsa.PrivateKey, error) {
 	return parsedKey, nil
 }
 
-func makeECPrivateKeyToPem(pk *ecdsa.PrivateKey) ([]byte, error) {
+func MakeECPrivateKeyToPem(pk *ecdsa.PrivateKey) ([]byte, error) {
 	parsed, err := x509.MarshalECPrivateKey(pk)
 	if err != nil {
 		return nil, err
@@ -61,7 +61,71 @@ func getECPublicKeyFromPem(secret []byte) (*ecdsa.PublicKey, error) {
 	return parsedKey.(*ecdsa.PublicKey), nil
 }
 
-func makeECPublicKeyToPem(pk *ecdsa.PublicKey) ([]byte, error) {
+func MakeECPublicKeyToPem(pk *ecdsa.PublicKey) ([]byte, error) {
+	block, err := x509.MarshalPKIXPublicKey(pk)
+	if err != nil {
+		return nil, err
+	}
+
+	rs := pem.EncodeToMemory(&pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: block,
+	})
+
+	return rs, nil
+}
+
+func getEd25519PrivateKeyFromPem(secret []byte) (ed25519.PrivateKey, error) {
+	block, _ := pem.Decode(secret)
+	if block == nil {
+		return nil, errors.New("invalid secret")
+	}
+
+	if block.Type != "PRIVATE KEY" {
+		return nil, errors.New("secret is not private key")
+	}
+
+	parsedKey, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return parsedKey.(ed25519.PrivateKey), nil
+}
+
+func MakeEd25519PrivateKeyToPem(pk ed25519.PrivateKey) ([]byte, error) {
+	block, err := x509.MarshalPKCS8PrivateKey(pk)
+	if err != nil {
+		return nil, err
+	}
+
+	rs := pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: block,
+	})
+
+	return rs, nil
+}
+
+func getEd25519PublicKeyFromPem(secret []byte) (ed25519.PublicKey, error) {
+	block, _ := pem.Decode(secret)
+	if block == nil {
+		return nil, errors.New("invalid secret")
+	}
+
+	if block.Type != "PUBLIC KEY" {
+		return nil, errors.New("secret is not public key")
+	}
+
+	parsedKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return parsedKey.(ed25519.PublicKey), nil
+}
+
+func MakeEd25519PublicKeyToPem(pk ed25519.PublicKey) ([]byte, error) {
 	block, err := x509.MarshalPKIXPublicKey(pk)
 	if err != nil {
 		return nil, err
@@ -87,6 +151,7 @@ func NewEd25519Key() (ed25519.PublicKey, ed25519.PrivateKey, error) {
 	return ed25519.GenerateKey(rand.Reader)
 }
 
-func NewEd25519KeyFromSeed(seed []byte) (ed25519.PrivateKey, error) {
-	return ed25519.NewKeyFromSeed(seed), nil
+func NewEd25519KeyFromSeed(seed []byte) (ed25519.PublicKey, ed25519.PrivateKey, error) {
+	privateKey := ed25519.NewKeyFromSeed(seed)
+	return privateKey.Public().(ed25519.PublicKey), privateKey, nil
 }
